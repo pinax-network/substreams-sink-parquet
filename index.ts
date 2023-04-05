ee770450b248679b906fad9e80d0731d1e708bf7
Denis Carriere <carriere.denis@gmail.com>
2023-04-04 22:18:34 -0400

    remove explicit type name

--- index.ts
+++ index.ts
@@ -36,9 +36,7 @@ export async function action(manifest: string, moduleName: string, options: Acti
     const writer = await parquet.ParquetWriter.openFile(schema, options.out);
 
     substreams.on("anyMessage", async (message: EntityChanges, clock: Clock, typeName: string) => {
-         // Only support for EntityChanges
-        if (typeName != TYPE_NAME ) return;
-
+        // Only supports for EntityChanges
         for ( const entityChange of message?.entityChanges || []) {
             const json = entityChange.toJson() as any;
             const fields: any = {};
